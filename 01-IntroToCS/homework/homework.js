896fbbcc7dfa68cd0f063598b6d7779dcc4b105b
laura-e24 <72033353+laura-e24@users.noreply.github.com>
2022-09-13 14:32:13 -0300

    initial commit

---
+++ 01-IntroToCS/homework/homework.js
@@ -0,0 +1,27 @@
+'use strict'
+
+function BinarioADecimal(num) {
+  // tu codigo aca
+  const binary = num.split('')
+
+  return binary.reduce((acc, val, i, arr) => (
+    acc + (val * 2**(arr.length - i - 1))
+  ), 0)
+}
+
+function DecimalABinario(num) {
+  // tu codigo aca
+  let binary = []
+
+  while (Math.floor(num) !== 0) {
+    binary.unshift(Math.floor(num) % 2)
+    num = num / 2
+  }
+  return binary.join('')
+}
+
+
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+}
